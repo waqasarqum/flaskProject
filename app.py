eff3621944ab418c5499d19a8ef3bb8feb657f34
waqas <waqy_arqum@hotmail.co.uk>
2022-07-14 16:11:13 +0100

    updated code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Waqas!'
+    return render_template("index.html")
 
 
 
