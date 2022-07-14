257d6f03d7152af9a4be511d193a6764613a3dce
waqas <waqy_arqum@hotmail.co.uk>
2022-07-14 14:42:04 +0100

    Hello Waqas

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello Waqas!'
 
 
 if __name__ == '__main__':
     app.run()
+
